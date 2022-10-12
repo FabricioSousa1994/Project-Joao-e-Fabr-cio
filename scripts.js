ca5d8008995fd31ca54f9bba261500f0021a434f
João Silva <joao.figueiredosilva@protonmail.com>
2022-10-12 20:05:21 +0100

    adicionei a musica e mudei um pouco o layout

--- scripts.js
+++ scripts.js
@@ -1,105 +1,120 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-let interval;
 
-const gameArea = {
-    start: () => {
-        interval = setInterval(updateGameArea, 20);
-    },
-    stop: () => {
-        clearInterval(interval)
-    },
-    clear: () => {
-        context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+const audio = document.querySelector("audio");
+audio.play();
+
+let audioIsPlaying = true;
+
+
+
+
+function isAudioPlaying() {
+    if (audioIsPlaying === true) {
+        audio.pause();
+        audioIsPlaying = false;
+    } else {
+        audio.play();
+        audioIsPlaying = true;
     }
 }
 
+let interval;
+
+const gameArea = {
+  start: () => {
+    interval = setInterval(updateGameArea, 20);
+  },
+  stop: () => {
+    clearInterval(interval);
+  },
+  clear: () => {
+    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  },
+};
+
 class Char {
-    constructor(x,y,height, width, ctx) {
+  constructor(x, y, height, width, ctx) {
     this.x = x;
     this.y = y;
     this.height = height;
     this.width = width;
     this.ctx = ctx;
     this.image = image;
-}
+  }
 
-draw() {
-    context.drawImage(this.image, this.x, this.y, 50, 50);
-}
+  draw() {
+    context.drawImage(this.image, this.x, this.y, 120, 100);
+  }
 
-moveUp() {
-    this.y -= 15
-}
+  moveUp() {
+    this.y -= 15;
+  }
 
-moveDown() {
-    this.y += 15
-}
+  moveDown() {
+    this.y += 15;
+  }
 
-moveLeft() {
-    this.x -= 15
-}
+  moveLeft() {
+    this.x -= 15;
+  }
 
-moveRight() {
-    this.x += 15
-}
+  moveRight() {
+    this.x += 15;
+  }
 
-left() {
+  left() {
     return this.x;
   }
 
   right() {
-      return this.x + this.image.width;
+    return this.x + this.image.width;
   }
 
   top() {
-      return this.y;
+    return this.y;
   }
 
   bottom() {
-      return this.y + this.image.height;
+    return this.y + this.image.height;
   }
-
 }
 
 document.addEventListener("keydown", (event) => {
-      console.log( "im clicicking", event.key)
-      switch (event.key) {
-        case "ArrowLeft":
-              zePovinho.moveLeft();
-            break;
-        case "ArrowRight":
-              zePovinho.moveRight();
-            break;
-        case "ArrowDown":
-            zePovinho.moveDown();
-            break;
-        case "ArrowUp":
-            zePovinho.moveUp()
-      }
-      context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-      zePovinho.draw();
-    
-  })
+  switch (event.key) {
+    case "ArrowLeft":
+      zePovinho.moveLeft();
+    break;
+    case "ArrowRight":
+      zePovinho.moveRight();
+    break;
+    case "ArrowDown":
+      zePovinho.moveDown();
+    break;
+    case "ArrowUp":
+      zePovinho.moveUp();
+    break;
+  }
+  context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  zePovinho.draw();
+});
 
-
-let image = new Image()
+let image = new Image();
 window.onload = () => {
   image.src = "/char.png";
-  document.getElementById('startGame').onclick = () => {
+  document.getElementById("startGame").onclick = () => {
     startGame();
   };
-}
+};
 
 
-const zePovinho = new Char(230,750, image, context)
+const zePovinho = new Char(230, 690, image, context);
 function startGame() {
-  zePovinho.draw()
+  zePovinho.draw();
   gameArea.start();
 }
 
-
 function updateGameArea() {
-    gameArea.clear();
-    zePovinho.draw();
+  gameArea.clear();
+  zePovinho.draw();
 }
